c2f9e531f5737e0461761745f981892c3a1e6803
HuskyKapsRepo <117322518+HuskyKapsRepo@users.noreply.github.com>
2023-08-23 17:14:54 -0600

    Add files via upload

---
+++ Lets_Chat_Room_Page.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDFeCM_-r_POqypAzNnnXO6e6a0GLyJDk4",
+    authDomain: "let-s-chat-67c8f.firebaseapp.com",
+    projectId: "let-s-chat-67c8f",
+    storageBucket: "let-s-chat-67c8f.appspot.com",
+    messagingSenderId: "902444555972",
+    appId: "1:902444555972:web:c6658175050645f9997391"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
